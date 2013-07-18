e764c27ca7d3391e28cfcfed76ad63ac34a646ed
Van Michael <van.michael.nguyen@gmail.com>
2013-07-18 17:25:34 -0400

    validate uniqueness of scores

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,12 +10,19 @@ class PlayersController < ApplicationController
 
 	def create
 		@player = Player.new(params[:player])
+		@user = User.find(current_user)
+		@player_exist = Player.find_by_user_id(current_user)
 		if @player.save
 			flash[:success] = "Score Posted Successfully"
 			redirect_to '/game'
 		else
-			flash[:error] = "Score not Posted, Please Try Again!"
-			redirect_to '/game'
+			if @player_exist != nil
+				flash[:error] = "Already Posted Score, nice try!"
+				redirect_to '/game'
+			else
+				flash[:error] = "Score not Posted, Please Try Again!"
+				redirect_to '/game'
+			end
 		end
 	end
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,7 @@
 class Player < ActiveRecord::Base
   attr_accessible :user_id, :score_one, :score_two
   belongs_to :user
+
+  validates_uniqueness_of :user_id
 end
 
